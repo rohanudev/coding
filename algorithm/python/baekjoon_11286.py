5fa975a2e80931669ab416c74ec6e9438414c19f
Hyunwoo Roh <rohanu.dev@gmail.com>
2025-05-01 14:12:13 +0900

    Update baekjoon_11286.py

--- algorithm/python/baekjoon_11286.py
+++ algorithm/python/baekjoon_11286.py
@@ -1,4 +1,16 @@
-import sys, heapq
+import sys, heapq, math
 
 input = sys.stdin.readline
+abs_heap = []
+num = int(input().strip())
 
+for _ in range(num):
+    tmp = int(input().strip())
+    if tmp:
+        heapq.heappush(abs_heap,(abs(tmp), tmp))
+    else:
+        if abs_heap:
+            ans = heapq.heappop(abs_heap)
+            print(ans[1])
+        else:
+            print(0)
